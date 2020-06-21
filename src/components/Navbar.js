af36b9dd1ddf7f595ae32566f532bc9aebda951a
Kevin Jensen <kevinjensen1133@yahoo.com>
2020-06-21 11:31:33 -0700

    testing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,13 @@ import styled from 'styled-components';
 
 export default function Navbar() {
     return (
-        <div className = "navbar" >
-            <h1>hello from Navbar</h1>
-        </div>
+        <Nav>
+            <div className = "navbar" >
+                <h1>hello from Navbar</h1>
+            </div>
+        </Nav>
     );
 }
-
+const Nav = styled.nav `
+    font-size: 7rem;
+`
