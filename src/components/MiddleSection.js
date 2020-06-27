a837cb716527ce5c670c28ba05ba6d54a56358a9
Kevin Jensen <kevinjensen1133@yahoo.com>
2020-06-27 09:00:32 -0700

    ok im done

--- src/components/MiddleSection.js
+++ src/components/MiddleSection.js
@@ -70,11 +70,12 @@ const Middle = styled.section `
             font-size: 2rem;
             width: 45%;
             
-            text-align: center;
+            text-align: left;
             @media ${device.tablet} {
                 width: 95%;
                 font-size: 1.6rem;
                 margin-top: 3%;
+                text-align: center;
             }
         }
     }
